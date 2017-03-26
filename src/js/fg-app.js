4a3439788a0c95b47f4fd8979c3ed6e58cdf6fca
Kevin Frutiger <github@frutigergroup.com>
2017-03-25 22:03:43 -0700

    Decouple 747 animation from app logic. Revise skybox code.
    - move animation sequence setup into fg-fly-pattern component and attach to
    boeing_747-100-flying entity
    - revise skybox append code in fg-app to resolve component errors

--- src/js/fg-app.js
+++ src/js/fg-app.js
@@ -6,24 +6,27 @@ AFRAME.registerComponent('fg-app', {
     if (!this.inAltspace()) {
       // Create a skybox.
       var sky = document.createElement('a-sky');
-      sky.setAttribute('material', 'color', 'blue');
-      sky.setAttribute('radius', '4096');
 
       this.el.appendChild(sky);
-    }
 
-    this.setupAnimation();
+      // For some reason, attributes have to be set after the sky is appended.
+      // Otherwise errors are thrown about components not existing.
+
+      // Set color to a natural sky color.
+      sky.setAttribute('material', 'color', '#0088ff');
+      // Set radius to match Altspace's fullspace skybox.
+      sky.setAttribute('geometry', 'radius', '4096');
+    }
   },
 
+  /**
+   * Returns whether document is running in the AltspaceVR client.
+   */
   inAltspace: function() {
     return (
       (typeof altspace !== 'undefined') &&
       (typeof altspace.inClient !== 'undefined') &&
       altspace.inClient
     );
-  },
-
-  setupAnimation: function() {
-    //
   }
 });
--- src/js/fg-fly-pattern.js
+++ src/js/fg-fly-pattern.js
@@ -1,32 +1,44 @@
+/**
+ * Manages the animation chain for flying around.
+ *
+ *  Expects at least two <a-animation> IDs. The first one (the upwind leg)
+ *  will emit 'flydownwind' on animationend and the second (the downind leg)
+ *  will emit 'flyupwind' on animationend (both on the main element).
+ *
+ *  The animation starts as soon as 'model-loaded' event fires on the element.
+ */
 AFRAME.registerComponent('fg-fly-pattern', {
-  schema: {
-    animations: { type: 'string' }
+  schema: { // Array of <a-animation> ids
+    type: 'array'
   },
 
   update: function() {
-    //var el = this.el;
-    //console.log(this.data)
-    //var boeing747 = document.querySelector('#boeing-747-100-flying');
-    // var upwind = el.querySelector('#' + this.data.animation_ids[0]);
-    // var downwind = el.querySelector('#' + this.data.animation_ids[1]);
+    var el = this.el;
 
-    // el.addEventListener('stateadded', function(event) {
-    //   if (event.detail.state === 'fly') {
-    //     el.emit('flyupwind');
-    //   }
-    // });
+    // Get references for our two required <a-animation> objects
+    var upwind = el.querySelector('#' + this.data[0]);
+    var downwind = el.querySelector('#' + this.data[1]);
 
-    // upwind.addEventListener('animationend', function() {
-    //   el.emit('flydownwind');
-    // });
+    // Listen for a 'fly' event then start the animation.
+    el.addEventListener('stateadded', function(event) {
+      if (event.detail.state === 'fly') {
+        el.emit('flyupwind');
+      }
+    });
 
-    // downwind.addEventListener('animationend', function() {
-    //   el.emit('flyupwind');
-    // });
+    // Listen for animation to end to start the other leg
+    upwind.addEventListener('animationend', function() {
+      el.emit('flydownwind');
+    });
 
-    // el.addEventListener('model-loaded', function() {
-    //   el.addState('fly');
-    // });
+    downwind.addEventListener('animationend', function() {
+      el.emit('flyupwind');
+    });
+
+    // Listen for the model to load then notify the main element to start.
+    el.addEventListener('model-loaded', function() {
+      el.addState('fly');
+    });
   }
 
 });
